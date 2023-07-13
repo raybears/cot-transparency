975c99c8c8029914af2fccce7f5b17429a60bdd0
jameschua <chuajamessh@gmail.com>
2023-07-12 19:58:32 -0700

    fix pydantic

--- cot_transparency/openai_utils/models.py
+++ cot_transparency/openai_utils/models.py
@@ -1,3 +1,4 @@
+from dataclasses import dataclass
 from enum import Enum
 from typing import Optional, Union, Dict, Any
 
@@ -19,7 +20,7 @@ class OpenaiInferenceConfig(BaseModel):
     max_tokens: int
     frequency_penalty: float = 0.0
     presence_penalty: float = 0.0
-    stop: Union[None, str, conlist(str, min_items=1, max_items=4)] = None  # type: ignore
+    stop: Union[None, str, conlist(str, min_length=1, max_length=4)] = None  # type: ignore
 
 
 class TokenProba(BaseModel):
@@ -33,7 +34,7 @@ class TokenInfo(BaseModel):
     text_offset: int  # the offset of the token in the text
     # the top 5 tokens in the probability distribution
     # for first token in the prompt this is empty
-    top_5_tokens: SlistPydantic[TokenProba]
+    top_5_tokens: list[TokenProba]
 
 
 class FinishReasons(str, Enum):
@@ -50,8 +51,6 @@ class OpenaiRoles(str, Enum):
     assistant_preferred = "assistant_preferred"
 
 
-
-
 class ChatMessages(BaseModel):
     role: OpenaiRoles
     content: str
@@ -62,14 +61,14 @@ class GPTFullResponse(BaseModel):
     # list of ChatMessages if its a chat api
     prompt: str | list[ChatMessages]
     completion: str
-    prompt_token_infos: SlistPydantic[TokenInfo]
-    completion_token_infos: SlistPydantic[TokenInfo]
+    prompt_token_infos: list[TokenInfo]
+    completion_token_infos: list[TokenInfo]
     completion_total_log_prob: float
     average_completion_total_log_prob: Optional[float]
     finish_reason: FinishReasons
 
     @property
-    def token_infos(self) -> SlistPydantic[TokenInfo]:
+    def token_infos(self) -> list[TokenInfo]:
         return self.prompt_token_infos + self.completion_token_infos
 
     @property
@@ -78,13 +77,12 @@ class GPTFullResponse(BaseModel):
 
     @property
     def average_completion_prob(self) -> Optional[float]:
-        completion_token_infos_log_prob: Slist[float] = self.completion_token_infos.map(
+        completion_token_infos_log_prob: Slist[float] = Slist(self.completion_token_infos).map(
             lambda token_info: token_info.log_prob
         )
         # convert them into probabilities and then average them
         probas: Slist[float] = completion_token_infos_log_prob.map(lambda log_prob: np.exp(log_prob))
         return probas.average()
-
 
 
 def parse_chat_prompt_response_dict(
@@ -123,7 +121,6 @@ def __get_chat_response_dict(
         stream=False,
         stop=[config.stop] if isinstance(config.stop, str) else config.stop,
     )
-
 
 
 @retry(
