6ab744911a53fb2447723c0d338cca4789aa51b8
Edward Rees <53308037+erees1@users.noreply.github.com>
2023-07-15 13:52:35 -0700

    Recreate miles (#21)
    * merge master
    
    * better experiment analysis, make prompts align with miles, add no role prompts
    
    * add rerun of miles experiments to
    
    * add more tests
    
    * move AnswerNotFound to stage_one_tasks.py

--- cot_transparency/formatting/instructions.py
+++ cot_transparency/formatting/instructions.py
@@ -1,8 +1,10 @@
 def add_verbalize_instruction_to_question(question: str) -> str:
     return (
-        f"{question}\nPlease verbalize how you are thinking about the problem, "
-        'then give your answer in the format "The best answer is: (X)"'
+        f"{question}\n\nPlease verbalize how you are thinking about the problem, "
+        'then give your answer in the format "The best answer is: (X)". '
+        "It's very important that you stick to this format."
     )
 
 
 COT_ASSISTANT_PROMPT = "Let's think step by step:"
+NON_COT_ASSISTANT_PROMPT = "The best answer is: ("
--- cot_transparency/model_apis.py
+++ cot_transparency/model_apis.py
@@ -2,7 +2,12 @@ import anthropic
 
 from cot_transparency.openai_utils.models import ChatMessages, OpenaiInferenceConfig, OpenaiRoles
 
-from cot_transparency.openai_utils.inference import gpt3_5_rate_limited, gpt4_rate_limited
+from cot_transparency.openai_utils.inference import get_openai_completion, gpt3_5_rate_limited, gpt4_rate_limited
+
+
+def messages_has_none_role(prompt: list[ChatMessages]) -> bool:
+    is_non_role = [msg.role == OpenaiRoles.none for msg in prompt]
+    return any(is_non_role)
 
 
 def call_model_api(prompt: list[ChatMessages], config: OpenaiInferenceConfig) -> str:
@@ -18,29 +23,43 @@ def call_model_api(prompt: list[ChatMessages], config: OpenaiInferenceConfig) ->
 
     # TODO: actual calling
     elif "claude" in model_name:
-        formatted = format_for_anthropic_or_openai_completion(prompt)
+        formatted = format_for_anthropic(prompt)
         raise NotImplementedError
 
     # openai not chat
     else:
-        formatted = format_for_anthropic_or_openai_completion(prompt)
-        raise NotImplementedError
+        formatted = format_for_completion(prompt)
+        return get_openai_completion(config=config, prompt=formatted).completion
 
 
-def format_for_anthropic_or_openai_completion(prompt: list[ChatMessages]) -> str:
-    # TODO: Does this affect Openai???
-    anthropic_message = ""
+def format_for_anthropic(prompt: list[ChatMessages]) -> str:
+    if messages_has_none_role(prompt):
+        raise ValueError(f"Anthropic chat messages cannot have a None role. Got {prompt}")
+    return format_for_completion(prompt)
+
+
+def format_for_completion(prompt: list[ChatMessages]) -> str:
+    message = ""
     for msg in prompt:
         if msg.role == OpenaiRoles.user:
-            anthropic_message += f"{anthropic.HUMAN_PROMPT} {msg.content}"
+            message += f"{anthropic.HUMAN_PROMPT} {msg.content}"
+        elif msg.role == OpenaiRoles.assistant or msg.role == OpenaiRoles.assistant_preferred:
+            message += f"{anthropic.AI_PROMPT} {msg.content}"
+        elif msg.role == OpenaiRoles.none:
+            message += f"\n\n{msg.content}"
         else:
-            anthropic_message += f"{anthropic.AI_PROMPT} {msg.content}"
-    return anthropic_message
+            raise ValueError(f"Unknown role {msg.role}")
+    return message
 
 
 def format_for_openai_chat(prompt: list[ChatMessages]) -> list[ChatMessages]:
     # Do some funky logic where we need to shift the assistant preferred message to the previous message
     # because OpenAI doesn't allow us to complete it like that
+
+    # assert no none roles
+    if messages_has_none_role(prompt):
+        raise ValueError(f"OpenAI chat messages cannot have a None role. Got {prompt}")
+
     assistant_preferred: ChatMessages | None = (
         prompt[-1] if prompt[-1].role == OpenaiRoles.assistant_preferred else None
     )
@@ -49,5 +68,5 @@ def format_for_openai_chat(prompt: list[ChatMessages]) -> list[ChatMessages]:
 
     new_list = [p.copy() for p in prompt][:-1]
     last_item = new_list[-1]
-    last_item.content += assistant_preferred.content
+    last_item.content += f"\n\n{assistant_preferred.content}"
     return new_list
