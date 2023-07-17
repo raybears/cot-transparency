d680c4c4d2c257f2fd1225a70a0537f05e6db710
jameschua <chuajamessh@gmail.com>
2023-07-17 12:01:28 -0700

    achange to warning instead

--- analysis.py
+++ analysis.py
@@ -89,8 +89,7 @@ def accuracy(
     accuracy_df = pivot_df(accuracy_df)
 
     if check_counts and not counts_are_equal(counts_df):
-        print("Counts are not equal for some tasks and their baselines, likely experiments not completed")
-        # exit(1)
+        print("WARNING: Counts are not equal for some tasks and their baselines, likely experiments not completed")
 
     print("---------------- Counts ----------------")
     print(counts_df)
