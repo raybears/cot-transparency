2b37126538af69a211423cc7107163104316523f
jameschua <chuajamessh@gmail.com>
2023-07-24 22:59:00 -0700

    add abstract class to basemodel

--- cot_transparency/data_models/example_base.py
+++ cot_transparency/data_models/example_base.py
@@ -1,3 +1,4 @@
+from abc import ABC, abstractmethod
 from typing import Literal
 from pydantic import BaseModel
 
@@ -6,17 +7,20 @@ from cot_transparency.util import deterministic_hash
 MultipleChoiceAnswer = Literal["A", "B", "C", "D", "E", "F", "NOT_FOUND"]
 
 
-class DataExampleBase(BaseModel):
+class DataExampleBase(BaseModel, ABC):
     """We don't define the fields here because we want to be able to use this for any dataset but we define the api"""
 
     @property
+    @abstractmethod
     def ground_truth(self) -> MultipleChoiceAnswer:
         raise NotImplementedError
 
     @property
+    @abstractmethod
     def biased_ans(self) -> MultipleChoiceAnswer:
         raise NotImplementedError
 
+    @abstractmethod
     def get_parsed_input(self) -> str:
         raise NotImplementedError
 
