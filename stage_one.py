82b8f39a90cb84b102c30839d0dd2ce1e13a0f46
erees1 <edward.r.rees@zoho.com>
2023-07-30 17:44:41 -0700

    added mmlu

--- stage_one.py
+++ stage_one.py
@@ -10,7 +10,7 @@ from cot_transparency.data_models.models import OpenaiInferenceConfig, TaskSpec
 
 from cot_transparency.formatters.base_class import StageOneFormatter
 
-from cot_transparency.data_models.data import aqua, arc, bbh, truthful_qa, logiqa
+from cot_transparency.data_models.data import aqua, arc, bbh, truthful_qa, logiqa, mmlu
 from cot_transparency.data_models.models import ChatMessage
 from cot_transparency.openai_utils.set_key import set_keys_from_env
 from cot_transparency.formatters import ZeroShotCOTSycophancyFormatter, ZeroShotCOTUnbiasedFormatter
@@ -98,6 +98,8 @@ def get_list_of_examples(dataset: str, task: str) -> list[DataExampleBase]:
             data = truthful_qa.eval()
         elif task == "logiqa":
             data = logiqa.eval()
+        elif task == "mmlu":
+            data = mmlu.test(questions_per_task=20)
 
     if data is None:
         raise ValueError(f"dataset and or task is not valid. Valid datasets are {list(TASK_LIST.keys())}")
