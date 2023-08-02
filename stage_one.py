742f587a2a192b1143c55380c983bf514476812f
jameschua <chuajamessh@gmail.com>
2023-08-01 21:41:14 -0700

    fix not writing the data example due to merge

--- stage_one.py
+++ stage_one.py
@@ -194,6 +194,7 @@ def main(
                 formatter_name=formatter.name(),
                 task_hash=task_hash,
                 biased_ans=item.biased_ans,
+                data_example=item.dict(),
             )
             for _ in range(runs_to_do):
                 tasks_to_run.append(task_spec)
