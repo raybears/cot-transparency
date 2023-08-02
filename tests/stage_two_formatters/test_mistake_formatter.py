c04e2f1fe0952e2a2e02fa5125f6de99bc4cba9f
erees1 <edward.r.rees@zoho.com>
2023-08-01 19:53:44 -0700

    add stage two formatter tests

---
+++ tests/stage_two_formatters/test_mistake_formatter.py
@@ -0,0 +1,35 @@
+from cot_transparency.data_models.models import StrictChatMessage
+from cot_transparency.formatters.transparency.mistakes import (
+    FEW_SHOT_PROMPT,
+    FewShotGenerateMistakeFormatter,
+    START_PROMPT,
+)
+from cot_transparency.model_apis import convert_to_anthropic_str
+
+from tests.test_prompt_formatter import EMPIRE_OF_PANTS_EXAMPLE
+
+EXAMPLE_SENTENCE = "This is a sentence with some reasoning, 5 x 20 = 100."
+
+
+def test_mistake_formatter_anthropic():
+    prompt: list[StrictChatMessage] = FewShotGenerateMistakeFormatter.format_example(
+        EMPIRE_OF_PANTS_EXAMPLE.get_parsed_input(), EXAMPLE_SENTENCE
+    )
+
+    anthropic_format = convert_to_anthropic_str(prompt)
+
+    expected = f"""\n\n{FEW_SHOT_PROMPT}
+
+Human: {START_PROMPT}
+
+{EMPIRE_OF_PANTS_EXAMPLE.get_parsed_input()}
+
+Original sentence: {EXAMPLE_SENTENCE}
+
+Assistant: Sentence with mistake added:"""
+
+    assert anthropic_format == expected
+
+
+if __name__ == "__main__":
+    test_mistake_formatter_anthropic()
