f5c3c4326a2224e4e903d07a55fa1d6dbf3f3fc8
Edward Rees <53308037+erees1@users.noreply.github.com>
2023-08-03 14:29:40 -0700

    make text-davinci-003 deafult temp =1 (#79)

--- stage_one.py
+++ stage_one.py
@@ -53,7 +53,7 @@ TASK_LIST = {
 CONFIG_MAP = {
     "gpt-4": OpenaiInferenceConfig(model="gpt-4", temperature=1, max_tokens=1000, top_p=1.0),
     "gpt-3.5-turbo": OpenaiInferenceConfig(model="gpt-3.5-turbo", temperature=1, max_tokens=1000, top_p=1.0),
-    "text-davinci-003": OpenaiInferenceConfig(model="text-davinci-003", temperature=0.7, max_tokens=1000, top_p=1.0),
+    "text-davinci-003": OpenaiInferenceConfig(model="text-davinci-003", temperature=1, max_tokens=1000, top_p=1.0),
     "code-davinci-002": OpenaiInferenceConfig(model="code-davinci-002", temperature=1, max_tokens=1000, top_p=1.0),
     "text-davinci-002": OpenaiInferenceConfig(model="text-davinci-002", temperature=1, max_tokens=1000, top_p=1.0),
     "davinici": OpenaiInferenceConfig(model="davinci", temperature=1, max_tokens=1000, top_p=1.0),
