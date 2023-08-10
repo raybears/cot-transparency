0a29fb2b9e0b416de019d4eff2c1dedc6af223b7
jameschua <chuajamessh@gmail.com>
2023-08-10 16:59:06 -0700

    add paired consistency

--- cot_transparency/formatters/more_biases/baseline_more_few_shots.py
+++ cot_transparency/formatters/more_biases/baseline_more_few_shots.py
@@ -4,36 +4,18 @@ from typing import Optional, Type, final
 from cot_transparency.data_models.example_base import DataExampleBase
 from cot_transparency.data_models.models import ChatMessage, MessageRole
 from cot_transparency.formatters.base_class import StageOneFormatter
-from cot_transparency.formatters.consistency_prompting.few_shots import consistency_few_shot
+from cot_transparency.formatters.consistency_prompting.few_shots import consistency_few_shot, few_shots_to_sample
+from cot_transparency.formatters.core.sycophancy import ZeroShotCOTSycophancyFormatter
+from cot_transparency.formatters.core.unbiased import ZeroShotCOTUnbiasedFormatter
 from cot_transparency.formatters.extraction import extract_answer
 from cot_transparency.formatters.instructions import add_verbalize_instruction_to_question, COT_ASSISTANT_PROMPT
-
-
-class BaselineMoreCOTs(StageOneFormatter):
-    is_biased = True
-    is_cot = True
-
-    @staticmethod
-    def format_example(question: DataExampleBase) -> list[ChatMessage]:
-        with_instruction = (
-            consistency_few_shot.convert_to_completion_str()
-            + "\n"
-            + add_verbalize_instruction_to_question(question.get_parsed_input())
-        )
-        output = [
-            ChatMessage(role=MessageRole.user, content=with_instruction),
-            ChatMessage(role=MessageRole.assistant_if_completion, content=COT_ASSISTANT_PROMPT),
-        ]
-        return output
-
-    @staticmethod
-    def parse_answer(response: str) -> Optional[str]:
-        return extract_answer(response, dump_failed=False)
+from scripts.biased_wrong_ans import sample_few_shots_cot
 
 
 class Intervention(ABC):
-    def __init__(self, name: str):
-        self.name = name
+    @classmethod
+    def name(cls) -> str:
+        return cls.__name__
 
     @abstractmethod
     def hook(self, question: DataExampleBase, messages: list[ChatMessage]) -> list[ChatMessage]:
@@ -57,6 +39,40 @@ class Intervention(ABC):
 
             @classmethod
             def name(cls) -> str:
-                return f"{intervention_self.name}_{formatter.name()}"
+                return f"{intervention_self.name()}_{formatter.name()}"
 
         return NewFormatter
+
+
+def prepend_to_front_first_user_message(messages: list[ChatMessage], prepend: str) -> list[ChatMessage]:
+    """Prepend a string to the first user message."""
+    new_messages = []
+    for m in messages:
+        if m.role == MessageRole.user:
+            new_messages.append(ChatMessage(role=MessageRole.user, content=prepend + m.content))
+        else:
+            new_messages.append(m)
+    return messages
+
+
+class PairedConsistency(Intervention):
+    def __init__(self, n_shots: int):
+        self.n_shots = n_shots
+
+    def hook(self, question: DataExampleBase, messages: list[ChatMessage]) -> list[ChatMessage]:
+        new = prepend_to_front_first_user_message(
+            messages=messages,
+            prepend=sample_few_shots_cot(
+                few_shots_to_sample, seed=question.hash(), n=self.n_shots
+            ).convert_to_completion_str(),
+        )
+        return new
+
+
+five_shot_intervention = PairedConsistency(5)
+PairedConsistency_ZeroShotCOTSycophancyFormatter: Type[StageOneFormatter] = five_shot_intervention.intervene(
+    ZeroShotCOTSycophancyFormatter
+)
+PairedConsistency_ZeroShotCOTUnbiasedFormatter: Type[StageOneFormatter] = five_shot_intervention.intervene(
+    ZeroShotCOTUnbiasedFormatter
+)
