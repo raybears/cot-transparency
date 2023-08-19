00c09639f167db8e89df4f8a73df82cd5eb2633b
James Chua <chuajamessh@gmail.com>
2023-08-19 12:49:59 -0700

    fix bigbrain samples having potentially wrong cots

--- scripts/dump_big_brain_cot_data.py
+++ scripts/dump_big_brain_cot_data.py
@@ -32,19 +32,20 @@ def big_brain():
     all_read: Slist[TaskOutput] = read_whole_exp_dir(exp_dir="experiments/big_brain")
     # separate unbiased from biased
     unbiased, biased = all_read.split_by(lambda x: x.task_spec.formatter_name == ZeroShotCOTUnbiasedFormatter.name())
+    # get only the unbiased correctly answered stuff
+    unbiased_correct = unbiased.filter(lambda x: x.task_spec.ground_truth == x.first_parsed_response)
+
     # get only the successfully biased stuff
     success_fully_biased: Slist[TaskOutput] = biased.filter(lambda x: x.task_spec.biased_ans == x.first_parsed_response)
     # make unbiased a dict based on the task hash
-    unbiased_dict: dict[str, TaskOutput] = unbiased.map(lambda x: (x.task_spec.task_hash, x)).to_dict()
+    unbiased_dict: dict[str, TaskOutput] = unbiased_correct.map(lambda x: (x.task_spec.task_hash, x)).to_dict()
     # joined the successful biased with the unbiased
-    joined = success_fully_biased.map(
-        lambda x: (
-            x,
-            unbiased_dict[x.task_spec.task_hash],
-        )
-    )
+    joined: Slist[tuple[TaskOutput, TaskOutput]] = success_fully_biased.map(
+        lambda x: (x, unbiased_dict[x.task_spec.task_hash]) if x.task_spec.task_hash in unbiased_dict else None
+    ).flatten_option()
     # make a nicer basemodel that contains the biased question as a str, and unbiased COT as a str
     nicer = joined.map(lambda x: to_biased_question_unbiased_cot(x[0], x[1]))
+    print(f"Found {len(nicer)} examples to write")
     write_jsonl_file_from_basemodel(
         path=Path("data/bbh_big_brain_cots/data.jsonl"),
         basemodels=nicer,
