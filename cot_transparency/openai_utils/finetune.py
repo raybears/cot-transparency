7b0e303bfbc963179fa5f45e3a3e7c40033ec1d2
James Chua <chuajamessh@gmail.com>
2023-08-24 17:29:03 -0700

    initial finetune script

---
+++ cot_transparency/openai_utils/finetune.py
@@ -0,0 +1,143 @@
+import datetime
+import io
+import time
+from pathlib import Path
+from typing import Any
+
+import openai
+from pydantic import BaseModel
+from slist import Slist
+
+from cot_transparency.data_models.models import (
+    StrictChatMessage,
+    StrictMessageRole,
+    TaskOutput,
+    ChatMessage,
+    MessageRole,
+)
+from cot_transparency.formatters.interventions.few_shots_loading import get_correct_cots
+from cot_transparency.json_utils.read_write import write_jsonl_file_from_basemodel
+from cot_transparency.model_apis import Prompt, ModelType
+from cot_transparency.openai_utils.set_key import set_keys_from_env
+
+set_keys_from_env()
+
+
+class FineTuneParams(BaseModel):
+    model: str
+    # todo: the new api doesn't have these params???
+    # n_epochs: int = 1
+    # batch_size: int = 1
+    # learning_rate_multiplier: float = 1.0
+
+
+def join_assistant_preferred_to_completion(messages: list[ChatMessage], completion: str) -> list[ChatMessage]:
+    # If we had a previous assistant_if_completion message, we want to join it with the new completion
+    last_message = messages[-1]
+    if last_message.role == MessageRole.assistant_if_completion:
+        messages[-1] = ChatMessage(role=MessageRole.assistant, content=last_message.content + completion)
+    else:
+        messages.append(ChatMessage(role=MessageRole.assistant, content=completion))
+    return messages
+
+
+class FinetuneSample(BaseModel):
+    messages: list[StrictChatMessage]
+
+    @staticmethod
+    def from_task_output(task: TaskOutput) -> "FinetuneSample":
+        prompt_messages = task.task_spec.messages
+        joined = join_assistant_preferred_to_completion(
+            messages=prompt_messages, completion=task.inference_output.raw_response
+        )
+        strict = Prompt(messages=joined)
+        return FinetuneSample(messages=strict.get_strict_messages(ModelType.chat))
+
+
+class FinetuneJob(BaseModel):
+    model: str
+    id: str  # job id
+
+
+def wait_until_uploaded_file_id_is_ready(file_id: str) -> None:
+    while True:
+        file = openai.File.retrieve(file_id)
+        if file["status"] == "processed":
+            return
+        time.sleep(1)
+
+
+def wait_until_finetune_job_is_ready(finetune_job_id: str) -> str:
+    """Returns the fine tuned model id"""
+    while True:
+        finetune_job = openai.FineTuningJob.retrieve(finetune_job_id)
+        if finetune_job["status"] == "succeeded":
+            return finetune_job["fine_tuned_model"]
+        time.sleep(1)
+
+
+def list_finetunes() -> None:
+    finetunes = openai.FineTuningJob.list()
+    print(finetunes)
+
+
+def cancel_finetune(finetune_id: str) -> None:
+    print(openai.FineTuningJob.cancel(id=finetune_id))
+
+
+def run_finetune(params: FineTuneParams, samples: list[FinetuneSample]) -> str:
+    # get time for file name
+    now_time = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+    file_name = f"{params.model}-{now_time}.jsonl"
+    write_jsonl_path = Path(file_name)
+    # write to file
+    write_jsonl_file_from_basemodel(path=write_jsonl_path, basemodels=samples)
+    # write to buffer cos openai wants a buffer
+    buffer = io.StringIO()
+    for item in samples:
+        buffer.write(item.model_dump_json() + "\n")
+    buffer.seek(0)
+    file_upload_resp: dict[str, Any] = openai.File.create( # type: ignore[reportGeneralTypeIssues]
+        file=buffer,
+        purpose="fine-tune",
+        user_provided_filename=file_name,
+    )
+    file_id = file_upload_resp["id"]
+    print("Starting file upload")
+    wait_until_uploaded_file_id_is_ready(file_id=file_id)
+    print(f"Uploaded file to openai. {file_upload_resp}")
+    finetune_job_resp = openai.FineTuningJob.create(training_file=file_id, model=params.model)
+    print(f"Started finetune job. {finetune_job_resp}")
+    parsed_job_resp: FinetuneJob = FinetuneJob.model_validate(finetune_job_resp)
+    model_id: str = wait_until_finetune_job_is_ready(finetune_job_id=parsed_job_resp.id)
+    print(f"Fine tuned model id: {model_id}")
+    return model_id
+
+
+def example_main():
+    # example you can run
+    messages = [
+        FinetuneSample(
+            messages=[
+                StrictChatMessage(role=StrictMessageRole.user, content="hello"),
+                StrictChatMessage(role=StrictMessageRole.assistant, content="bye"),
+            ]
+        )
+    ] * 10
+    params = FineTuneParams(model="gpt-3.5-turbo")
+    run_finetune(params=params, samples=messages)
+
+
+def fine_tune_with_gpt4_cots():
+    cots: Slist[TaskOutput] = get_correct_cots()
+    messages = [FinetuneSample.from_task_output(task) for task in cots]
+    params = FineTuneParams(model="gpt-3.5-turbo")
+    _id = run_finetune(params=params, samples=messages)
+
+
+if __name__ == "__main__":
+    fine_tune_with_gpt4_cots()
+    # example_main()
+    # list_finetunes()
+    # cancel_finetune("ftjob-Or0LQD1IrKLh4fWOVcXQHYBS")
+    # wait_until_finetune_job_is_ready("ftjob-ktQb6K5b83PnJfpklFX9lGYr")
