0d0979307d28f83376e0b8b8ccc370f8dfb7e7a0
James Chua <chuajamessh@gmail.com>
2023-09-24 15:40:23 +0800

    edit experiment sample

--- scripts/run_consistency.py
+++ scripts/run_consistency.py
@@ -2,11 +2,15 @@ from cot_transparency.formatters.core.prompt_sensitivity_map import no_cot_sensi
 from stage_one import main
 
 if __name__ == "__main__":
-    # An
+    # Run the experiment for prompt sensitivity
     main(
         dataset="cot_testing",
         formatters=[f.name() for f in no_cot_sensitivity_formatters],
-        example_cap=10,
-        models=["gpt-3.5-turbo"],
+        example_cap=100,
+        models=[
+            "gpt-3.5-turbo",
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::813SHRdF",
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::81c693MV",
+        ],
         exp_dir="experiments/sensitivity",
     )
