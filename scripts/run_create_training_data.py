ec75957f9f775feefc8619c6eaa9e1ae8860fcb4
James Chua <chuajamessh@gmail.com>
2023-10-05 16:29:23 +0800

    add no cot to claude data gen

--- scripts/run_create_training_data.py
+++ scripts/run_create_training_data.py
@@ -1,10 +1,11 @@
-from cot_transparency.formatters.core.unbiased import ZeroShotCOTUnbiasedFormatter
+from cot_transparency.formatters.core.unbiased import ZeroShotCOTUnbiasedFormatter, ZeroShotUnbiasedFormatter
 from scripts.training_formatters import TRAINING_COT_FORMATTERS, TRAINING_NO_COT_FORMATTERS
 from stage_one import main
 
 if __name__ == "__main__":
     # Script to replicate generating training data
     # Run `export PYTHONPATH=.; python scripts/run_create_training_data.py`
+    # For big brain, dumb brain training
     exp_dir_gpt_35 = "experiments/training_data_temp_1"
     main(
         dataset="cot_training",
@@ -15,10 +16,21 @@ if __name__ == "__main__":
         exp_dir=exp_dir_gpt_35,
         batch=10,
     )
+    # For simple correct COT training
+    main(
+        dataset="cot_training",
+        formatters=[ZeroShotCOTUnbiasedFormatter.name(), ZeroShotUnbiasedFormatter.name()],
+        example_cap=5000,
+        models=["gpt-3.5-turbo"],
+        temperature=1.0,
+        exp_dir=exp_dir_gpt_35,
+        batch=20,
+    )
+
     exp_dir_claude_2 = "experiments/training_data_temp_1_claude_2_unbiased"
     main(
         dataset="cot_training",
-        formatters=[ZeroShotCOTUnbiasedFormatter.name()],
+        formatters=[ZeroShotCOTUnbiasedFormatter.name(), ZeroShotUnbiasedFormatter.name()],
         example_cap=5000,
         models=["claude-2"],
         temperature=1.0,
