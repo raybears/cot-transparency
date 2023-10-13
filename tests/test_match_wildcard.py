84d33caf046106e630767dd31c059a1652655ed1
James Chua <30519287+thejaminator@users.noreply.github.com>
2023-10-13 14:05:30 +0100

    Fix potential bug with mutating input lists (#217)
    * Fix potential bug with mutating input lists
    
    * simplify

---
+++ tests/test_match_wildcard.py
@@ -0,0 +1,5 @@
+from cot_transparency.formatters.wildcard import match_wildcard_formatters
+
+
+def test_match_wildcard_formatters():
+    assert match_wildcard_formatters(["ZeroShotUnbiasedFormatte*"]) == ["ZeroShotUnbiasedFormatter"]
