6bb6958647604c01c63004514d956898c86bb4c1
James Chua <30519287+thejaminator@users.noreply.github.com>
2023-10-23 12:21:42 +0100

    Tweak alignment tax script - use COT classifier, use harder H4 dataset (#231)
    * wip
    
    * wip
    
    * use h4 instead

---
+++ scripts/evaluate_alignment_tax/train_different_instruct_proportion.py
@@ -0,0 +1,40 @@
+from pydantic import BaseModel
+
+from cot_transparency.formatters.interventions.few_shots_loading import ModelOutputVerified
+from scripts.evaluate_alignment_tax.instruction_following import eval_instruction_following
+from scripts.finetune_cot import fine_tune_with_bias_augmentation, DataFromOptions, FormatterOptions
+
+import asyncio
+
+
+class SweepOptions(BaseModel):
+    n_samples: int
+    instruct_sample_proportion: float
+
+
+async def train_and_run(sweep: SweepOptions) -> None:
+    model = fine_tune_with_bias_augmentation(
+        model="gpt-3.5-turbo",
+        n_epochs=1,
+        n_samples=sweep.n_samples,
+        post_hoc=False,
+        cot_percentage=0.5,
+        data_from_options=DataFromOptions.gpt_35_turbo,
+        formatter_options=FormatterOptions.all_biased,
+        model_output_verified=ModelOutputVerified.correct,
+        ask_to_validate_training=False,
+        instruct_sample_proportion=sweep.instruct_sample_proportion,
+        prepend_notes="instruct_sample_proportion: " + str(sweep.instruct_sample_proportion) + " ",
+    )
+    await eval_instruction_following(
+        intervention_model=model,
+    )
+
+
+async def main():
+    option = SweepOptions(n_samples=1000, instruct_sample_proportion=0.1)
+    await train_and_run(option)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
---
+++ scripts/load_h4_dataset.py
@@ -0,0 +1,33 @@
+import dataclasses
+
+from datasets import load_dataset
+from slist import Slist
+
+from cot_transparency.data_models.messages import StrictChatMessage, StrictMessageRole
+from cot_transparency.apis.openai.finetune import FinetuneSample
+
+
+@dataclasses.dataclass
+class H4Samples:
+    test: Slist[FinetuneSample]
+
+
+def get_h4_test() -> H4Samples:
+    # Load the dataset
+    dataset = load_dataset("HuggingFaceH4/instruction-dataset")
+    test = dataset["test"]  # type: ignore
+    items = Slist()
+    for row in test:
+        user_prompt: str = row["prompt"]  # type: ignore
+        completion: str = row["completion"]  # type: ignore
+        user_msg = StrictChatMessage(role=StrictMessageRole.user, content=user_prompt)
+        assistant_msg = StrictChatMessage(role=StrictMessageRole.assistant, content=completion)
+        item = FinetuneSample(messages=[user_msg, assistant_msg])
+        items.append(item)
+
+    return H4Samples(test=items)
+
+
+if __name__ == "__main__":
+    output = get_h4_test()
+    print(output)
