af1ef566fee88a31805fc403f3a1261798f55308
erees1 <edward.r.rees@gmail.com>
2023-10-24 12:22:14 +0100

    train on variants updates

--- scripts/prompt_sen_bias_generalization/train_on_variants.py
+++ scripts/prompt_sen_bias_generalization/train_on_variants.py
@@ -2,6 +2,7 @@ from dataclasses import dataclass
 from enum import Enum
 import os
 import random
+from typing import Optional
 from dotenv import load_dotenv
 
 import openai
@@ -33,7 +34,7 @@ class SweepOptions:
     sampler: FormatSampler = RandomSampler()
 
 
-def train_and_run(sweep: SweepOptions) -> None:
+def train_and_run(sweep: SweepOptions, skip_evaluation: bool = False) -> None:
     # Train on prompt variants
     model = fine_tune_with_bias_augmentation(
         model="gpt-3.5-turbo",
@@ -44,27 +45,30 @@ def train_and_run(sweep: SweepOptions) -> None:
         data_from_options=DataFromOptions.gpt_35_turbo,
         formatter_options=sweep.formatter_options,
         model_output_verified=ModelOutputVerified.correct,
-        ask_to_validate_training=True,
+        ask_to_validate_training=False,
         instruct_sample_proportion=0.1,
         sampler=sweep.sampler,
     )
     # Test on both few shot and zero shot biases
-    test_formatters = [f.name() for f in TRAINING_COT_FORMATTERS]
-    stage_one_main(
-        exp_dir="experiments/finetune_3",
-        models=[model],
-        formatters=test_formatters,
-        dataset="cot_testing",
-        example_cap=400,
-        raise_after_retries=False,
-        temperature=1.0,
-    )
+    print(f"done training model {model}")
+    if skip_evaluation:
+        test_formatters = [f.name() for f in TRAINING_COT_FORMATTERS]
+        stage_one_main(
+            exp_dir="experiments/finetune_3",
+            models=[model],
+            formatters=test_formatters,
+            dataset="cot_testing",
+            example_cap=400,
+            raise_after_retries=False,
+            temperature=1.0,
+        )
 
 
 def main(
     n_samples: int = 100,
     format_options: str = FormatterOptions.prompt_variants_set1.value,
-    n_formats_per_question: int = 1,
+    n_formats_per_question: Optional[int] = None,
+    skip_evaluation: bool = False,
 ):
     load_dotenv()
     org = os.environ.get("OPENAI_ORG_IDS")
@@ -83,12 +87,14 @@ def main(
 
     formatter_options = FormatterOptions(format_options)
     print("Runing with", formatter_options, n_samples)
+
+    if n_formats_per_question is None:
+        sampler = RandomSampler()
+    else:
+        sampler = NFormatsPerQuestionSampler(n_formats_per_question=n_formats_per_question)
+
     train_and_run(
-        SweepOptions(
-            n_samples=n_samples,
-            formatter_options=formatter_options,
-            sampler=NFormatsPerQuestionSampler(n_formats_per_question=n_formats_per_question),
-        )
+        SweepOptions(n_samples=n_samples, formatter_options=formatter_options, sampler=sampler), skip_evaluation
     )
     print("done")
 
