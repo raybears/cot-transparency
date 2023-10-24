a937b72462cbd45a08fe65ffa7c9681e3c7d0931
James Chua <chuajamessh@gmail.com>
2023-10-24 19:51:17 +0100

    save error bars for plot

--- scripts/evaluate_alignment_tax/instruction_following.py
+++ scripts/evaluate_alignment_tax/instruction_following.py
@@ -2,14 +2,16 @@ import asyncio
 import random
 from enum import Enum
 from pathlib import Path
-from typing import Optional, Sequence
-
+from typing import Optional, Sequence, Mapping, List
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
 from grugstream import Observable
 from pydantic import BaseModel
 from slist import Slist
 from tqdm import tqdm
 
-from cot_transparency.apis import OpenAIChatCaller, get_caller
+from cot_transparency.apis import OpenAIChatCaller
 from cot_transparency.apis.base import Prompt, ModelCaller
 from cot_transparency.apis.openai import OpenAICompletionPrompt
 from cot_transparency.apis.openai.finetune import FinetuneSample
@@ -61,6 +63,20 @@ def alpaca_sample_to_prompt(sample: FinetuneSample) -> Prompt:
     # the last message is the one we want to predict
     messages_without_last = messages[:-1]
     return Prompt(messages=messages_without_last)
+
+
+class PromptWithModel(BaseModel):
+    prompt: Prompt
+    config: OpenaiInferenceConfig
+
+
+def alpaca_sample_to_prompts(
+    sample: FinetuneSample, intervention_models: list[OpenaiInferenceConfig]
+) -> list[PromptWithModel]:
+    out = []
+    for config in intervention_models:
+        out.append(PromptWithModel(prompt=alpaca_sample_to_prompt(sample=sample), config=config))
+    return out
 
 
 class QuestionWithLabels(BaseModel):
@@ -124,7 +140,7 @@ def get_judge_output(comparison: ComparisonGeneration, judge: ModelCaller) -> Co
 
 def eval_judged(judged: Sequence[ComparisonGenerationJudged]) -> None:
     judged_slist = Slist(judged).filter(
-        lambda j: abs(len(j.generation.vanilla_response) - len(j.generation.intervention_response)) <= 200
+        lambda j: abs(len(j.generation.vanilla_response) - len(j.generation.intervention_response)) <= 100
     )
     print(f"Total judged: {len(judged_slist)}")
     winner_vanilla = len(judged_slist.filter(lambda j: j.winner == JudgeChoice.vanilla))
@@ -146,12 +162,86 @@ def eval_judged(judged: Sequence[ComparisonGenerationJudged]) -> None:
     print(f"Total winner first: {length_first}")
 
 
-async def eval_instruction_following(intervention_model: str):
+class WinrateMetrics(BaseModel):
+    win_rate: float
+    se: float
+    samples: int
+
+
+def get_win_rate(judged: Sequence[ComparisonGenerationJudged]) -> WinrateMetrics:
+    judged_slist = Slist(judged).filter(
+        lambda j: abs(len(j.generation.vanilla_response) - len(j.generation.intervention_response)) <= 100
+    )
+    print(f"Total judged: {len(judged_slist)}")
+    winner_vanilla = len(judged_slist.filter(lambda j: j.winner == JudgeChoice.vanilla))
+    print(f"Total winner vanilla: {winner_vanilla}")
+    winner_intervention = len(judged_slist.filter(lambda j: j.winner == JudgeChoice.intervention))
+    print(f"Total winner intervention: {winner_intervention}")
+    # Intervention win rate
+    win_rate = winner_intervention / (winner_vanilla + winner_intervention)
+    # get the standard error
+    se = (win_rate * (1 - win_rate) / (winner_vanilla + winner_intervention)) ** 0.5
+    return WinrateMetrics(win_rate=win_rate, se=se, samples=winner_vanilla + winner_intervention)
+
+
+def win_rate_plot(
+    judged: Sequence[ComparisonGenerationJudged], sort_by: Sequence[str], rename_map: Mapping[str, str]
+) -> None:
+    win_rates: Slist[tuple[str, WinrateMetrics]] = (
+        (
+            Slist(judged)
+            .group_by(
+                # group by model
+                lambda j: j.generation.intervention_config.model,
+            )
+            .map_2(
+                # get win rate
+                lambda model, judged: (model, get_win_rate(judged)),
+            )
+        )
+        .sort_by(
+            # sort by model
+            lambda model_win_rate: sort_by.index(model_win_rate[0]),
+        )
+        .map(
+            # rename
+            lambda model_win_rate: (rename_map.get(model_win_rate[0], model_win_rate[0]), model_win_rate[1]),
+        )
+    )
+    # use seaborn
+
+    # create a dataframe
+    list_dicts = [
+        {"model": model, "win_rate": win_rate.win_rate, "se": win_rate.se, "samples": win_rate.samples}
+        for model, win_rate in win_rates
+    ]
+
+    # errors: list[float] = [win_rate.se for _, win_rate in win_rates]
+    df = pd.DataFrame(list_dicts)
+    ax = sns.barplot(x="model", y="win_rate", data=df)
+
+    # set y axis to 0-1
+    plt.ylim(0, 1)
+    # add standard error bars
+    plt.errorbar(x=df["model"], y=df["win_rate"], yerr=df["se"], fmt="none", capsize=0.1, color="black")
+
+    # x-axis should be "Percentage of additional instruction dataset samples"
+    # title should be "Win rate of intervention models against gpt-3.5-turbo"
+    ax.set(xlabel="Percentage of additional instruction dataset samples", ylabel="Win rate")
+    ax.set_title("Win rate of intervention models against gpt-3.5-turbo on the Huggingface Instruction Eval dataset")
+    # add a red dotted line at 50%
+    plt.axhline(y=0.5, color="r", linestyle="--")
+
+    plt.show()
+
+
+async def eval_instruction_following(intervention_models: list[str]):
     # ft:gpt-3.5-turbo-0613:academicsnyuperez::8B24hv5w 10k samples, 0% instruction
     # ft:gpt-3.5-turbo-0613:academicsnyuperez::89ghXobC 100k samples, 10% instruction
     samples: Slist[FinetuneSample] = get_h4_test().test
     print(f"Total testing samples: {len(samples)}")
-    intervention_caller = get_caller(model_name=intervention_model)().with_file_cache(
+
+    intervention_caller = OpenAIChatCaller().with_file_cache(
         cache_path=Path("experiments/alignment_tax/intervention_completion.jsonl"), write_every_n=10
     )
     vanilla_caller = OpenAIChatCaller().with_file_cache(
@@ -159,20 +249,24 @@ async def eval_instruction_following(intervention_model: str):
     )
     judge_model = OpenAIChatCaller().with_file_cache(Path("experiments/alignment_tax/judge.jsonl"), write_every_n=10)
     vanilla_config = OpenaiInferenceConfig(model="gpt-3.5-turbo", max_tokens=1000, temperature=0.0, top_p=1.0)
-    intervention_config = OpenaiInferenceConfig(model=intervention_model, max_tokens=1000, temperature=0.0, top_p=1.0)
+    intervention_configs = [
+        OpenaiInferenceConfig(model=intervention_model, max_tokens=1000, temperature=0.0, top_p=1.0)
+        for intervention_model in intervention_models
+    ]
 
+    prompts: Slist[PromptWithModel] = samples.map(
+        lambda sample: alpaca_sample_to_prompts(sample, intervention_configs)
+    ).flatten_list()
     pipeline = (
-        Observable.from_iterable(samples)
-        .map(alpaca_sample_to_prompt)
+        Observable.from_iterable(prompts)
         .map_blocking_par(
-            lambda prompt: generate_comparison(
-                prompt=prompt,
+            lambda with_model: generate_comparison(
+                prompt=with_model.prompt,
                 vanilla_caller=vanilla_caller,
                 intervention_caller=intervention_caller,
                 vanilla_config=vanilla_config,
-                intervention_config=intervention_config,
-            ),
-            max_par=20,
+                intervention_config=with_model.config,
+            )
         )
         .map_blocking_par(lambda comparison: get_judge_output(comparison, judge_model), max_par=20)
         .tqdm(tqdm(total=samples.length))
@@ -188,7 +282,31 @@ async def eval_instruction_following(intervention_model: str):
     vanilla_caller.save_cache()
     judge_model.save_cache()
     eval_judged(results)
+    win_rate_plot(
+        results,
+        sort_by=intervention_models,
+        rename_map={
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDdvsrO": "0% added",
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CEGJGjq": "10% added",
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CE4CPmg": "100% added",
+            "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDxzKfb": "1000% added",
+        },
+    )
 
 
 if __name__ == "__main__":
-    asyncio.run(eval_instruction_following(intervention_model="ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDdvsrO"))
+    # 100k ft:gpt-3.5-turbo-0613:academicsnyuperez::89ghXobC, 10 % ft:gpt-3.5-turbo-0613:academicsnyuperez::89ghXobC
+    # 1k ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDxzKfb 10x instruct
+    # 1k ft:gpt-3.5-turbo-0613:academicsnyuperez::8CE4CPmg 1x instruct
+    # 1k ft:gpt-3.5-turbo-0613:academicsnyuperez::8CEGJGjq 0.1x instruct
+    # 1k ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDdvsrO 0x instruct
+    asyncio.run(
+        eval_instruction_following(
+            intervention_models=[
+                "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDdvsrO",
+                "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CEGJGjq",
+                "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CE4CPmg",
+                # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8CDxzKfb",
+            ]
+        )
+    )
