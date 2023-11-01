222565388e8a2709696d3c23b311ed5a478c20b5
James Chua <30519287+thejaminator@users.noreply.github.com>
2023-11-01 15:24:27 +0800

    Allow the streamlit viewer to view the jsonl cache of a ModelCaller (#245)

--- cot_transparency/viewer/util.py
+++ cot_transparency/viewer/util.py
@@ -1,11 +1,13 @@
-from collections.abc import Sequence
-from typing import cast
+import pathlib
+from typing import cast, Sequence
 
 import streamlit as st
+from slist import Slist
 
+from cot_transparency.apis.base import FileCacheRow
 from cot_transparency.apis.openai import append_assistant_preferred_to_last_user
 from cot_transparency.data_models.messages import ChatMessage, MessageRole
-from cot_transparency.data_models.models import TaskOutput
+from cot_transparency.data_models.models import TaskOutput, ModelOutput, TaskSpec
 
 
 def display_messages(messages: Sequence[ChatMessage]):
@@ -53,3 +55,24 @@ def display_task(task: TaskOutput, put_if_completion_in_user: bool = True):
     )
     messages = list(messages) + [ChatMessage(role=MessageRole.assistant, content=task.inference_output.raw_response)]
     display_messages(messages)
+
+
+def file_cache_row_to_task_output(row: FileCacheRow) -> Sequence[TaskOutput]:
+    responses: Sequence[str] = row.response.response.raw_responses
+    task_hash = Slist(row.response.messages).map(str).mk_string("")
+    task_spec = TaskSpec(
+        task_name="not_used",
+        inference_config=row.response.config,
+        messages=row.response.messages,
+        out_file_path=pathlib.Path("not_used"),
+        ground_truth="A",
+        formatter_name="not_used",
+        task_hash=task_hash,
+    )
+    return [
+        TaskOutput(
+            task_spec=task_spec,
+            inference_output=ModelOutput(raw_response=response, parsed_response=None),
+        )
+        for response in responses
+    ]
