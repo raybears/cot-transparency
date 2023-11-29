68a75e325f513166f13e384a378a51a5ae88a0dc
erees1 <edward.r.rees@gmail.com>
2023-11-29 17:54:21 +0000

    formatting and use read_jsonl_file_into_basemodel

--- cot_transparency/data_models/data/refusal.py
+++ cot_transparency/data_models/data/refusal.py
@@ -1,13 +1,9 @@
-from typing import Optional, List
 from collections import Counter
-from abc import ABC, abstractmethod
+from cot_transparency.json_utils.read_write import read_jsonl_file_into_basemodel
 
-import pandas as pd
-import jsonlines
 from slist import Slist
 
-from cot_transparency.data_models.example_base import DataExampleBase
-from cot_transparency.data_models.example_base import MultipleChoiceAnswer
+from cot_transparency.data_models.example_base import DataExampleBase, MultipleChoiceAnswer
 
 
 class RefusalExample(DataExampleBase):
@@ -16,37 +12,25 @@ class RefusalExample(DataExampleBase):
 
     def _get_question(self) -> str:
         return self.question.strip()
-    
+
     def _get_options(self) -> list[str]:
         return [""]
-    
+
     @property
     def _ground_truth(self) -> MultipleChoiceAnswer:
-        return self.correct_ans_letter
-
+        return "A"  # dummy answer to make the code work
 
 
 def load_data(questions_per_category: int = 20) -> Slist[RefusalExample]:
-    path = f"./data/refusal/filtered_questions.jsonl"
-    with open(path, "r") as f:
-        data = jsonlines.Reader(f)
-        data = list(data)
+    path = "./data/refusal/filtered_questions.jsonl"
+    data = read_jsonl_file_into_basemodel(path, RefusalExample)
 
     outputs = Slist()
     questions_per_category_counter = Counter()
-    for i, line in enumerate(data):
-        print(line)
-        question: str = line["question"]  # type: ignore
-        category: str = line["category"]
-        questions_per_category_counter[category] += 1
-
-        example = RefusalExample(
-            question=question,
-            category=category,
-        )
-        if questions_per_category_counter[category] <= questions_per_category:
+    for i, example in enumerate(data):
+        if questions_per_category_counter[example.category] <= questions_per_category:
             outputs.append(example)
-        
+
     return outputs
 
 
