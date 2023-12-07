b81ec652b9e4daa602b25c6eff55d09615178d76
erees1 <edward.r.rees@gmail.com>
2023-12-07 14:46:46 +0000

    streamline eval the grid

--- scripts/evil_grid_exp/eval_the_grid.py
+++ scripts/evil_grid_exp/eval_the_grid.py
@@ -28,21 +28,6 @@ def accuracy_for_biases(tasks: Slist[TaskOutput]) -> Slist[Group[str, float]]:
     return grouped
 
 
-def accuracy_improvement_over_control(
-    intervention_model: str,
-    control_model: str,
-    tasks: Slist[TaskOutput],
-) -> Slist[Group[str, float]]:
-    """More positive is better"""
-    intervention_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == intervention_model)
-    control_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == control_model)
-    intervention_accuracy = accuracy_for_biases(intervention_tasks)
-    control_accuracy = accuracy_for_biases(control_tasks).to_dict()
-    return intervention_accuracy.map(
-        lambda group: group.map_values(lambda group_val: group_val - control_accuracy[group.key])
-    )
-
-
 def answer_matching_for_biases(tasks: Slist[TaskOutput]) -> Slist[Group[str, float]]:
     # group by formatter
     # need to filter out to get those that has the bias on the wrong answer for so grug don't need to brain so much
@@ -78,64 +63,46 @@ INTERESTING_FORMATTERS_STR = [x.name() for x in INTERESTING_FORMATTERS]
 
 
 def answer_matching_intervention_vs_control_csv(
-    intervention_model: str,
-    control_model: str,
+    models: dict[str, str],
     tasks: Slist[TaskOutput],
 ) -> None:
     """More negative is better"""
-    intervention_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == intervention_model)
-    control_tasks: Slist[TaskOutput] = tasks.filter(lambda x: x.task_spec.inference_config.model == control_model)
-    intervention_matching: Slist[Group[str, float]] = answer_matching_for_biases(intervention_tasks).sort_by(
-        lambda x: INTERESTING_FORMATTERS_STR.index(x.key)
-    )
-    control_matching: Slist[Group[str, float]] = answer_matching_for_biases(control_tasks).sort_by(
-        lambda x: INTERESTING_FORMATTERS_STR.index(x.key)
-    )
-    # Make a csv of two columns. Intervention and control
-    # values are the group values
-    row = []
-    for intervention, group in zip(intervention_matching, control_matching):
-        row.append(
-            {
-                "Formatter": intervention.key,
-                f"Intervention({intervention_model})": intervention.values,
-                f"Control({control_model})": group.values,
-            }
+
+    out: dict[str, dict[str, float]] = {}
+    for name, model in models.items():
+        filtered_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == model)
+        matching = (
+            answer_matching_for_biases(filtered_tasks)
+            .sort_by(lambda x: INTERESTING_FORMATTERS_STR.index(x.key))
+            .to_dict()
         )
-    df = pd.DataFrame(row)
+        heading_name = f"{name} (model ending {model[-6:]})"
+        out[heading_name] = matching
+
+    df = pd.DataFrame(out)
     df.to_csv("grid_exp_separate_answer_matching.csv")
 
 
 def accuracy_intervention_vs_control_csv(
-    intervention_model: str,
-    control_model: str,
+    models: dict[str, str],
     tasks: Slist[TaskOutput],
 ) -> None:
     """More positive is better"""
-    intervention_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == intervention_model)
-    control_tasks: Slist[TaskOutput] = tasks.filter(lambda x: x.task_spec.inference_config.model == control_model)
-    intervention_matching: Slist[Group[str, float]] = accuracy_for_biases(intervention_tasks).sort_by(
-        lambda x: INTERESTING_FORMATTERS_STR.index(x.key)
-    )
-    control_matching: Slist[Group[str, float]] = accuracy_for_biases(control_tasks).sort_by(
-        lambda x: INTERESTING_FORMATTERS_STR.index(x.key)
-    )
-    # Make a csv of two columns. Intervention and control
-    # values are the group values
-    row = []
-    for intervention, group in zip(intervention_matching, control_matching):
-        row.append(
-            {
-                "Formatter": intervention.key,
-                f"Intervention({intervention_model})": intervention.values,
-                f"Control({control_model})": group.values,
-            }
+
+    out: dict[str, dict[str, float]] = {}
+    for name, model in models.items():
+        filtered_tasks = tasks.filter(lambda x: x.task_spec.inference_config.model == model)
+        matching = (
+            accuracy_for_biases(filtered_tasks).sort_by(lambda x: INTERESTING_FORMATTERS_STR.index(x.key)).to_dict()
         )
-    df = pd.DataFrame(row)
+        heading_name = name + " Model ending: " + model[-6:]
+        out[heading_name] = matching
+
+    df = pd.DataFrame(out)
     df.to_csv("grid_exp_separate_accuracy.csv")
 
 
-async def eval_when_done(control: str, intervention: str) -> None:
+async def eval_grid(models: dict[str, str]) -> None:
     # FAR
     # openai.organization = "org-AFgHGbU3MeFr5M5QFwrBET31"
     stage_one_path = Path("experiments/grid_exp")
@@ -157,7 +124,7 @@ async def eval_when_done(control: str, intervention: str) -> None:
         caller=stage_one_caller,
         batch=40,
         # control model is ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ
-        models=[control, intervention],
+        models=list(models.values()),
     )
     results = await stage_one_obs.to_slist()
     # save results
@@ -168,38 +135,12 @@ async def eval_when_done(control: str, intervention: str) -> None:
     # dump to jsonl so the viewer can see it
     write_jsonl_file_from_basemodel("appendix.jsonl", results)
 
-    stats: Slist[Group[str, float]] = accuracy_improvement_over_control(
-        intervention_model=intervention,
-        control_model=control,
-        tasks=results,
-    )
-    print(stats)
-    # create a df and csv
-    _dict: dict[str, float] = stats.to_dict()
-    # ValueError: If using all scalar values, you must pass an index
-    df = pd.DataFrame.from_dict(_dict, orient="index").reset_index()
-    df.to_csv("grid_exp_acc.csv")
-
-    stats_matching = answer_matching_improvement_over_control(
-        intervention_model=intervention,
-        control_model=control,
-        tasks=results,
-    )
-    print(stats_matching)
-    # create a df and csv
-    _dict_matching: dict[str, float] = stats_matching.to_dict()
-    # ValueError: If using all scalar values, you must pass an index
-    df_matching = pd.DataFrame.from_dict(_dict_matching, orient="index").reset_index()
-    df_matching.to_csv("grid_exp_matching.csv")
-
     answer_matching_intervention_vs_control_csv(
-        intervention_model=intervention,
-        control_model=control,
+        models,
         tasks=results,
     )
     accuracy_intervention_vs_control_csv(
-        intervention_model=intervention,
-        control_model=control,
+        models,
         tasks=results,
     )
 
@@ -214,61 +155,60 @@ if __name__ == "__main__":
     "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ"
     # wandb https://wandb.ai/raybears/consistency-training/runs/ehcof6tv?workspace=user-chuajamessh
     """
-    asyncio.run(
-        eval_when_done(
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNz4qzi",
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ"
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8ODyGVgA",  # control lr 3.2
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8OE5l8Hf",  # intervention lr 3.2
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy, 10k
-            # control="gpt-3.5-turbo-0613",
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy, 10k
-            # control="ft:gpt-3.5-turbo-0613:far-ai::8PsCvzzt", # control 100k
-            # control="ft:gpt-3.5-turbo-0613:far-ai::8PMWz1KH", # model generated sycophancy 10k, exact repeats
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8PdiHkxT", # model generated sycophancy 100k,sampled 10 repeats
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8PMWz1KH",  # repeat 10 exact times the same
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8PxwywqE",  # 10 different times, model generated sycophancy 10k
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8PMYNDtK",  # 10 different times, model generated sycophancy 10k
-            #  start big brain
-            # control="ft:gpt-3.5-turbo-0613:far-ai::8NhzkHGU", # random bias c1ontrol 1k
-            control="ft:gpt-3.5-turbo-0613:far-ai::8S9b2Nn7",  # 50-50 cot 8.2k
-            intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8Heb2m",  # 95-5 noncot
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8SQUpNkC", # posthoc only
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8Heb2m", # 10k 95% biased non-cot, 5% unbiased cot
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8QdJtq3b", # all zeroshot
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8N1Ln5", # "Retrained only sycophancy variants 10k"
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8Rv34IGI",  # Paraphrase COT too 10k
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8RqwhLli",  # Trained on James' paraphrasings
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8NhzCN9o", # random bias intervention 1k
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Nq8QN2g",  # big brain's control 1k
-            # control= "ft:gpt-3.5-turbo-0613:far-ai::8NhzCN9o",  # model generated sycophancy 1k
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Nhwi79b",  # big brain's intervention 1k
-            # start hunars stuff
-            # control = "ft:gpt-3.5-turbo-0613:academicsnyuperez:mistake-70-30-10k:8OQQXtqS", # 10k
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez:mistake-0-100-10k:8OQPNX7p",  # 10k
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez:logiqa-0-100-1k:8LBCYXh3",
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez:logiqa-70-30-1k:8Mf9goC5",
-            # end
-            # control="gpt-3.5-turbo-0613",
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",  # THE OG CONTROL
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NY2C1j7" # wrogn few shot and i think the anser is (X)
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NYN7QsN", # wrong  few shot
-            # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNz4qzi",  # Combined paraphrasing + all zero shot formatters
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNFqGeq",  # paraphrasing only
-            # intervention="ft:gpt-3.5-turbo-0613:far-ai::8NPtWM2y"  # All Zero shot formatters only
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NmbzJp0", # paraphrasing: model sycophancy and spurious context
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NhdoGRg",  # unbiased on cot biased on no cot
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NmbzJp0"  # on the fly paraphrasing model
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8OC4213p"  # paraphrasing: model sycophancy (bios)
-            # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NYN7QsN",  # wrong few shot ignore mistakes
-            # models=[
-            #     # "gpt-3.5-turbo-0613",
-            #     "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",  # control 10k
-            #     "ft:gpt-3.5-turbo-0613:academicsnyuperez::8N6zCcpf",  # stanford
-            #     # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7RGEik",  # i think answer is (x) sycophancy
-            # ]
-        )
+    models = dict(
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNz4qzi",
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ"
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8ODyGVgA",  # control lr 3.2
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8OE5l8Hf",  # intervention lr 3.2
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy, 10k
+        # control="gpt-3.5-turbo-0613",
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy, 10k
+        # control="ft:gpt-3.5-turbo-0613:far-ai::8PsCvzzt", # control 100k
+        # control="ft:gpt-3.5-turbo-0613:far-ai::8PMWz1KH", # model generated sycophancy 10k, exact repeats
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8PdiHkxT", # model generated sycophancy 100k,sampled 10 repeats
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8PMWz1KH",  # repeat 10 exact times the same
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8PxwywqE",  # 10 different times, model generated sycophancy 10k
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8PMYNDtK",  # 10 different times, model generated sycophancy 10k
+        #  start big brain
+        # control="ft:gpt-3.5-turbo-0613:far-ai::8NhzkHGU", # random bias c1ontrol 1k
+        control="ft:gpt-3.5-turbo-0613:far-ai::8S9b2Nn7",  # 50-50 cot 8.2k
+        intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8Heb2m",  # 95-5 noncot
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8SQUpNkC", # posthoc only
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8Heb2m", # 10k 95% biased non-cot, 5% unbiased cot
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8QdJtq3b", # all zeroshot
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8S8N1Ln5", # "Retrained only sycophancy variants 10k"
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8Rv34IGI",  # Paraphrase COT too 10k
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8RqwhLli",  # Trained on James' paraphrasings
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8NhzCN9o", # random bias intervention 1k
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Nq8QN2g",  # big brain's control 1k
+        # control= "ft:gpt-3.5-turbo-0613:far-ai::8NhzCN9o",  # model generated sycophancy 1k
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Nhwi79b",  # big brain's intervention 1k
+        # start hunars stuff
+        # control = "ft:gpt-3.5-turbo-0613:academicsnyuperez:mistake-70-30-10k:8OQQXtqS", # 10k
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez:mistake-0-100-10k:8OQPNX7p",  # 10k
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez:logiqa-0-100-1k:8LBCYXh3",
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez:logiqa-70-30-1k:8Mf9goC5",
+        # end
+        # control="gpt-3.5-turbo-0613",
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",  # THE OG CONTROL
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NY2C1j7" # wrogn few shot and i think the anser is (X)
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NYN7QsN", # wrong  few shot
+        # control="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNz4qzi",  # Combined paraphrasing + all zero shot formatters
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NNFqGeq",  # paraphrasing only
+        # intervention="ft:gpt-3.5-turbo-0613:far-ai::8NPtWM2y"  # All Zero shot formatters only
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7p2hsv",  # model generated sycophancy
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NmbzJp0", # paraphrasing: model sycophancy and spurious context
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NhdoGRg",  # unbiased on cot biased on no cot
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NmbzJp0"  # on the fly paraphrasing model
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8OC4213p"  # paraphrasing: model sycophancy (bios)
+        # intervention="ft:gpt-3.5-turbo-0613:academicsnyuperez::8NYN7QsN",  # wrong few shot ignore mistakes
+        # models=[
+        #     # "gpt-3.5-turbo-0613",
+        #     "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",  # control 10k
+        #     "ft:gpt-3.5-turbo-0613:academicsnyuperez::8N6zCcpf",  # stanford
+        #     # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8N7RGEik",  # i think answer is (x) sycophancy
+        # ]
     )
+    asyncio.run(eval_grid(models))
