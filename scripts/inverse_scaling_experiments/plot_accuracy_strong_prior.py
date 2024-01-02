121b2065d5a9d5d3c5dd25acdd2f042fe5343b4b
James Chua <chuajamessh@gmail.com>
2024-01-02 18:07:48 +0800

    wip

--- scripts/inverse_scaling_experiments/plot_accuracy_strong_prior.py
+++ scripts/inverse_scaling_experiments/plot_accuracy_strong_prior.py
@@ -17,7 +17,7 @@ from scripts.intervention_investigation import plot_for_intervention
 
 
 from enum import Enum
-from typing import Sequence
+from typing import Optional, Sequence
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -34,7 +34,7 @@ class Method(str, Enum):
 
 class ModelTrainMeta(BaseModel):
     name: str
-    trained_samples: float
+    proportion_instruct_data: float
     trained_on: Method
 
 
@@ -54,70 +54,48 @@ def read_metric_from_meta(
     return ModelNameAndTrainedSamplesAndMetrics(train_meta=meta, metrics=acc.acc)
 
 
+
 def samples_meta() -> Slist[ModelTrainMeta]:
     # fill this up from wandb https://wandb.ai/raybears/consistency-training?workspace=user-chuajamessh
     """
-    [
-        "gpt-3.5-turbo-0613",
-        # instruct 0.0 (control)
-        "ft:gpt-3.5-turbo-0613:far-ai::8MEjCxQd",
-        # instruct 0.0
-        "ft:gpt-3.5-turbo-0613:far-ai::8MEm4sSK",
-        # instruct 0.1 (control)
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8MEVuHFu",
-        # instruct 0.1
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8MEw9gAq",
-        # instruct 1.0 (control)
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lw0sYjQ",
-        # isntruct 1.0
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lywfnnz",
-        # instruct 10.0 (control)
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Lk3VEOY",
-        # instruct 10.0 (ours)
-        "ft:gpt-3.5-turbo-0613:academicsnyuperez::8LpkPY5V",
-    ]
+    _100k_0_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aauYoO9",
+    _100k_1_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aanfHwN",
+    _100k_5_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aj7xOvu",
+    _100k_10_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8ab2bFlv",
+    _100k_25_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aDG4tqK",
+    _100k_50_perc_new="ft:gpt-3.5-turbo-0613:academicsnyuperez::8aDRJnSG",
+    _100k_100_perc_new="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aCHrIH2",
     """
     all_meta = Slist(
         [
             ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UgveEcS",
-                trained_samples=0,
-                trained_on=Method.control,
-            ),
-            ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8Uh02hU7",
-                trained_samples=0,
+                name="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aCHrIH2",
+                proportion_instruct_data=0,
                 trained_on=Method.ours,
-            ),
-            ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UgJoLOk",
-                trained_samples=0.1,
-                trained_on=Method.control,
             ),
             ModelTrainMeta(
                 name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UfaPIrG",
-                trained_samples=0.1,
-                trained_on=Method.ours,
-            ),
-            # Unbiased contexts
-            ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UN5nhcE",
-                trained_samples=1,
-                trained_on=Method.control,
-            ),
-            ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UNAODuA",
-                trained_samples=1,
+                proportion_instruct_data=0.5,
                 trained_on=Method.ours,
             ),
             ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8V6lCiZJ",
-                trained_samples=10,
-                trained_on=Method.control,
+                name="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aDG4tqK",
+                proportion_instruct_data=0.75,
+                trained_on=Method.ours,
             ),
             ModelTrainMeta(
-                name="ft:gpt-3.5-turbo-0613:academicsnyuperez::8UpgSQAf",
-                trained_samples=10,
+                name="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8ab2bFlv",
+                proportion_instruct_data=0.9,
+                trained_on=Method.ours,
+            ),
+            ModelTrainMeta(
+                name="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aj7xOvu",
+                proportion_instruct_data=0.95,
+                trained_on=Method.ours,
+            ),
+            ModelTrainMeta(
+                name="ft:gpt-3.5-turbo-0613:james-cot-transparency-org::8aanfHwN",
+                proportion_instruct_data=0.99,
                 trained_on=Method.ours,
             ),
         ]
@@ -134,42 +112,18 @@ def read_all_metrics(
     return samples.map(lambda meta: read_metric_from_meta(meta, all_tasks))
 
 
-def convert_to_linear_scale(x: float) -> int:
-    # hack to make it evenly spaced
-    if x == 0:
-        return 0
-    if x == 0.1:
-        return 1
-    if x == 1:
-        return 2
-    if x == 10:
-        return 3
-    else:
-        raise ValueError(f"Unknown value {x}")
-
-
-def linear_scale_to_string(x: int) -> str:
-    if x == 0:
-        return "0 x"
-    if x == 1:
-        return "0.1 x"
-    if x == 2:
-        return "1 x"
-    if x == 3:
-        return "10 x"
-    else:
-        raise ValueError(f"Unknown value {x}")
 
 
 def seaborn_line_plot(
     data: Sequence[ModelNameAndTrainedSamplesAndMetrics],
     error_bars: bool = True,
+    gpt_35_accuracy: Optional[float] = None,
 ):
     y_axis_label = "Accuracy %"
     df = pd.DataFrame(
         [
             {
-                "Additional Instruct Samples": convert_to_linear_scale(i.train_meta.trained_samples),
+                "Instruct Tuning Sample Proportion": i.train_meta.proportion_instruct_data,
                 y_axis_label: i.metrics.accuracy,
                 "Error Bars": i.metrics.error_bars,
                 "Training method": i.train_meta.trained_on.value,
@@ -179,38 +133,49 @@ def seaborn_line_plot(
     )
     df[y_axis_label] = df[y_axis_label]
 
-    sns.lineplot(data=df, x="Additional Instruct Samples", y=y_axis_label, hue="Training method")
+    ax= sns.lineplot(data=df, x="Instruct Tuning Sample Proportion", y=y_axis_label)
+
+    if gpt_35_accuracy is not None:
+        # draw a horizontal line at the gpt-3.5 accuracy
+        # make sure it appears in the legend
+        ax.axhline(y=gpt_35_accuracy, color="red", linestyle="--", label="GPT-3.5 accuracy")
+        # Add a legend
+        ax.legend()
 
     if error_bars:
         for name, group in df.groupby("Training method"):
             plt.errorbar(
-                group["Additional Instruct Samples"],
+                group["Instruct Tuning Sample Proportion"],
                 group[y_axis_label],
                 yerr=group["Error Bars"],
                 fmt="none",
                 capsize=5,
                 ecolor="black",
             )
-    # plt.xticks(df["Additional Instruct Samples"].unique())  # type: ignore
+    plt.xticks([0, 0.5, 0.75, 0.99], ["0%", "50%", "75%", "99%"])
+    
     # rotate xticks slightly
     # plt.xticks(rotation=-15)
-    # set x ticks to unique
-    # plt.xticks(df["Additional Instruct Samples"].unique())
+    # set x tickss to unique
+    # plt.xticks(df["Instruct Tuning Sample Proportion"].unique())
     # replot the xticks with linear_scale_to_string
-    plt.xticks(
-        df["Additional Instruct Samples"].unique(),  # type: ignore
-        [linear_scale_to_string(i) for i in df["Additional Instruct Samples"].unique()],
-    )
+    # plt.xticks(
+    #     df["Instruct Tuning Sample Proportion"].unique(),  # type: ignore
+    #     [linear_scale_to_string(i) for i in df["Instruct Tuning Sample Proportion"].unique()],
+    # )
 
     plt.ylim(0, 1)
     # Mutliple y-axis ticks by 100
-    plt.yticks([0, 0.2, 0.4, 0.6, 0.8, 1.0], Slist([0, 20, 40, 60, 80, 100]).map(str))
+    # plt.yticks([0, 0.2, 0.4, 0.6, 0.8, ], Slist([0, 20, 40, 60, 80, 100]).map(str))
+    # make the x ticks the
 
     # x-axis log scale
     # plt.xscale("log")
     # title
-    # plt.title("Accuracy on Strong Prior tasks")
-    plt.show()
+    plt.title("Accuracy on Strong Prior tasks")
+    # plt.show()
+    # save as pdf
+    plt.savefig("strong_prior_tasks.pdf", bbox_inches="tight")
 
 
 async def plot_accuracies():
@@ -219,7 +184,7 @@ async def plot_accuracies():
     # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8MmNKzZh",  # ours (superdataset, without few shot)
     # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8KreNXFv", # control paraphrasing 10k
     # "ft:gpt-3.5-turbo-0613:academicsnyuperez::8Kb1ayZh" # ours paraphrasing 10k
-    models = [m.name for m in samples_meta()]
+    models = [m.name for m in samples_meta()] + ["gpt-3.5-turbo-0613"]
     stage_one_path = Path("experiments/inverse_scaling/stage_one.jsonl")
     stage_one_caller = UniversalCaller().with_file_cache(stage_one_path, write_every_n=1000)
     # task = InverseScalingTask.memo_trap
@@ -247,15 +212,19 @@ async def plot_accuracies():
 
     results: Slist[TaskOutput] = await stage_one_obs.to_slist()
 
-    grouped_by_model_and_formatter = results.group_by(
-        lambda x: (x.task_spec.inference_config.model, x.task_spec.formatter_name)
+    ori_gpt_35, other_model_results = results.split_by(
+        lambda x: x.task_spec.inference_config.model == "gpt-3.5-turbo-0613"
     )
-    counts = grouped_by_model_and_formatter.map(
-        lambda group: group.map_values(lambda x: x.map(lambda val: val.inference_output.parsed_response is None).sum())
-    ).to_dict()
 
-    for k, v in counts.items():
-        print(k, v)
+    # grouped_by_model_and_formatter = results.group_by(
+    #     lambda x: (x.task_spec.inference_config.model, x.task_spec.formatter_name)
+    # )
+    # counts = grouped_by_model_and_formatter.map(
+    #     lambda group: group.map_values(lambda x: x.map(lambda val: val.inference_output.parsed_response is None).sum())
+    # ).to_dict()
+
+    # for k, v in counts.items():
+    #     print(k, v)
 
     write_jsonl_file_from_basemodel("experiments/inverse_scaling/instruction_following.jsonl", results)
 
@@ -264,8 +233,11 @@ async def plot_accuracies():
     stage_one_caller.save_cache()
 
     defined_meta = samples_meta()
-    read_metrics = read_all_metrics(samples=defined_meta, all_tasks=results)
-    seaborn_line_plot(read_metrics)
+    read_metrics = read_all_metrics(samples=defined_meta, all_tasks=other_model_results)
+    # get accuracy for gpt-3.5-turbo-0613
+    ori_gpt_35_acc: PlotInfo = plot_for_intervention(ori_gpt_35)
+
+    seaborn_line_plot(read_metrics, gpt_35_accuracy=ori_gpt_35_acc.acc.accuracy)
 
 
 if __name__ == "__main__":
